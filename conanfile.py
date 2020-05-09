37c9f5da2d70272cc73780a68594d7043aa1687a
Matthew Rodusek <matthew.rodusek@gmail.com>
2020-05-08 23:13:00 -0400

    🔖 Update version to 1.0.0
    The completion of `variant` marked the last major milestone required
    for the 1.0.0 release of Backport! With this being completed, the
    various versioned files in this repository is being updated in
    preparation of a 1.0.0 release

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class BackportConan(ConanFile):
 
   # Package Info
   name = "Backport"
-  version = "0.2.0"
+  version = "1.0.0"
   description = "Modern C++ backported to C++11"
   url = "https://github.com/bitwizeshift/bpstd"
   author = "Matthew Rodusek <matthew.rodusek@gmail.com>"
